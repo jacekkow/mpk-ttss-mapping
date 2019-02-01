d0f949e7fdbc916b1725d6e8c87a6bf7df081639
Jacek Kowalski <Jacek@jacekk.info>
2019-02-01 02:13:27 +0100

    Do not download GTFS_KRK_A.zip anymore

--- parse.sh
+++ parse.sh
@@ -6,7 +6,7 @@ DIR=`pwd`
 
 cd "$DIR/data"
 wget -O vehicles_A.json "http://91.223.13.70/internetservice/geoserviceDispatcher/services/vehicleinfo/vehicles"
-wget -N "ftp://ztp.krakow.pl/GTFS_KRK_A.zip" "ftp://ztp.krakow.pl/VehiclePositions_A.pb"
+wget -N "ftp://ztp.krakow.pl/VehiclePositions_A.pb"
 
 cd "$DIR"
 php parse.php > data/mapping_A.tmp
