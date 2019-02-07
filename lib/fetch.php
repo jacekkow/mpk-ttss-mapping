4673cc38e7b5b1b87d8e009137fc2d5eae688fd7
Jacek Kowalski <Jacek@jacekk.info>
2019-02-08 00:27:25 +0100

    Get rid of bash helper that downloads files from FTP/HTTP
    Additionally only parse data when GTFS file has changed
    since last download. Some refactoring was also done.

---
+++ lib/fetch.php
@@ -0,0 +1,86 @@
+<?php
+function ftp_fetch_if_newer($url, $file = NULL) {
+	$url = parse_url($url);
+	if(!isset($url['scheme']) || $url['scheme'] != 'ftp') {
+		throw new Exception('Only FTP URLs are supported');
+	}
+	if(!isset($url['host'])) {
+		throw new Exception('Hostname not present in the URL');
+	}
+	if(!isset($url['path'])) {
+		throw new Exception('Path component not present in the URL');
+	}
+	if(!isset($url['port'])) {
+		$url['port'] = 21;
+	}
+	if(!isset($url['user'])) {
+		$url['user'] = 'anonymous';
+	}
+	if(!isset($url['pass'])) {
+		$url['pass'] = 'anonymous@mpk.jacekk.net';
+	}
+	if($file == NULL) {
+		$file = basename($url['path']);
+	}
+	
+	$localTime = -1;
+	$localSize = -1;
+	if(is_file($file)) {
+		$localTime = filemtime($file);
+		$localSize = filesize($file);
+	}
+	
+	$ftp = ftp_connect($url['host'], $url['port'], 10);
+	if($ftp === FALSE) {
+		throw new Exception('FTP connection failed');
+	}
+	if(!ftp_login($ftp, $url['user'], $url['pass'])) {
+		throw new Exception('FTP login failed');
+	}
+	if(!ftp_pasv($ftp, TRUE)) {
+		throw new Exception('Passive FTP request failed');
+	}
+	$remoteSize = ftp_size($ftp, $url['path']);
+	if($remoteSize < 0) {
+		throw new Exception('FTP file size fetch failed');
+	}
+	$remoteTime = ftp_mdtm($ftp, $url['path']);
+	if($remoteTime < 0) {
+		throw new Exception('FTP modification time fetch failed');
+	}
+	
+	$updated = FALSE;
+	
+	if($localSize != $remoteSize || $localTime < $remoteTime) {
+		if(file_exists($file.'.tmp')) {
+			unlink($file.'.tmp');
+		}
+		if(ftp_get($ftp, $file.'.tmp', $url['path'], FTP_BINARY)) {
+			touch($file.'.tmp', $remoteTime);
+			if(!rename($file.'.tmp', $file)) {
+				throw new Exception('Temporary file rename failed');
+			}
+			$updated = TRUE;
+		}
+	}
+	
+	ftp_close($ftp);
+	
+	return $updated;
+}
+
+function fetch($url, $file = NULL) {
+	if($file == NULL) {
+		$file = basename($url['url']);
+	}
+	$data = file_get_contents($url);
+	if($data === FALSE) {
+		throw new Exception('URL fetch failed');
+	}
+	if(file_put_contents($file.'.tmp', $data) === FALSE) {
+		throw new Exception('Temporary file creation failed');
+	}
+	if(!rename($file.'.tmp', $file)) {
+		throw new Exception('Temporary file rename failed');
+	}
+}
--- parse.php
+++ parse.php
@@ -1,142 +1,45 @@
 <?php
-require('vendor/autoload.php');
-require('common.php');
+require_once(__DIR__.'/lib/fetch.php');
+require_once(__DIR__.'/lib/mapper.php');
 
-use transit_realtime\FeedMessage;
+$logger = new Monolog\Logger('Parse changes');
 
-class IdMapper {
-	private $jsonTrips = [];
-	private $gtfsTrips = [];
-	
-	private $specialNames = [
-		'Zjazd do zajezdni',
-		'Przejazd techniczny',
-		'Wyjazd na trasę',
-	];
-	
-	public static function convertTripId($tripId) {
-		$tripId = explode('_', $tripId);
-		if($tripId[0] != 'block') return;
-		if($tripId[2] != 'trip') return;
-		return 4096 * (int)$tripId[1] + (int)$tripId[3];
-	}
-	
-	public function loadJson($file) {
-		$json = json_decode(file_get_contents($file));
-		foreach($json->vehicles as $vehicle) {
-			if(isset($vehicle->isDeleted) && $vehicle->isDeleted) continue;
-			if(!isset($vehicle->tripId) || !$vehicle->tripId) continue;
-			if(!isset($vehicle->name) || !$vehicle->name) continue;
-			if(!isset($vehicle->latitude) || !$vehicle->latitude) continue;
-			if(!isset($vehicle->longitude) || !$vehicle->longitude) continue;
-			foreach($this->specialNames as $name) {
-				if(substr($vehicle->name, -strlen($name)) == $name) {
-					continue;
-				}
-			}
-			$this->jsonTrips[(int)$vehicle->tripId] = [
-				'id' => $vehicle->id,
-				'latitude' => (float)$vehicle->latitude / 3600000.0,
-				'longitude' => (float)$vehicle->longitude / 3600000.0,
-			];
-		}
-		ksort($this->jsonTrips);
-	}
-	
-	public function loadGtfs($file) {
-		$data = file_get_contents($file);
-		$feed = new FeedMessage();
-		$feed->parse($data);
-		foreach ($feed->getEntityList() as $entity) {
-			$vehiclePosition = $entity->getVehicle();
-			$position = $vehiclePosition->getPosition();
-			$vehicle = $vehiclePosition->getVehicle();
-			$trip = $vehiclePosition->getTrip();
-			$tripId = $trip->getTripId();
-			$this->gtfsTrips[self::convertTripId($tripId)] = [
-				'id' => $entity->getId(),
-				'num' => $vehicle->getLicensePlate(),
-				'tripId' => $tripId,
-				'latitude' => $position->getLatitude(),
-				'longitude' => $position->getLongitude(),
-			];
-		}
-		ksort($this->gtfsTrips);
-	}
-	
-	public function findOffset() {
-		if(count($this->jsonTrips) == 0 || count($this->gtfsTrips) == 0) {
-			return NULL;
-		}
-		
-		$jsonTripIds = array_keys($this->jsonTrips);
-		$gtfsTripIds = array_keys($this->gtfsTrips);
-		
-		$possibleOffsets = [];
-		for($i = 0; $i < count($this->jsonTrips); $i++) {
-			for($j = 0; $j < count($this->gtfsTrips); $j++) {
-				$possibleOffsets[$jsonTripIds[$i] - $gtfsTripIds[$j]] = TRUE;
-			}
-		}
-		$possibleOffsets = array_keys($possibleOffsets);
-		
-		$bestOffset = 0;
-		$maxMatched = 0;
-		$options = 0;
-		
-		foreach($possibleOffsets as $offset) {
-			$matched = 0;
-			
-			foreach($gtfsTripIds as $tripId) {
-				$tripId += $offset;
-				if(isset($this->jsonTrips[$tripId])) {
-					$matched++;
-				}
-			}
-			
-			if($matched > $maxMatched) {
-				$bestOffset = $offset;
-				$maxMatched = $matched;
-				$options = 1;
-			} elseif($matched == $maxMatched) {
-				$options++;
-			}
-		}
-		
-		if($options != 1) {
-			fwrite(STDERR, 'Found '.$options.' possible mappings!'."\n");
-			return FALSE;
-		}
-		return $bestOffset;
-	}
-	
-	public function getMapping($offset) {
-		$result = [];
-		foreach($this->gtfsTrips as $gtfsTripId => $gtfsTrip) {
-			$jsonTripId = $gtfsTripId + $offset;
-			if(isset($this->jsonTrips[$jsonTripId])) {
-				$data = numToTypeB($gtfsTrip['id']);
-				$num = $gtfsTrip['num'];
-				if(!is_array($data) || !isset($data['num'])) {
-					$data = [
-						'num' => $num,
-						'low' => 2,
-					];
-				} elseif($data['num'] != $num) {
-					// Ignore due to incorrect depot markings in the data
-					//fwrite(STDERR, 'Got '.$num.', database has '.$data['num']."\n");
-				}
-				$result[$this->jsonTrips[$jsonTripId]['id']] = $data;
-			}
-		}
-		return $result;
-	}
-}
+$sources = [
+	'buses' => [
+		'gtfs' => 'ftp://ztp.krakow.pl/VehiclePositions_A.pb',
+		'gtfs_file' => 'VehiclePositions_A.pb',
+		'ttss' => 'http://91.223.13.70/internetservice/geoserviceDispatcher/services/vehicleinfo/vehicles',
+		'ttss_file' => 'vehicles_A.json',
+	],
+];
 
-$mapper = new IdMapper();
-$mapper->loadJson('./data/vehicles_A.json');
-$mapper->loadGtfs('./data/VehiclePositions_A.pb');
-$offset = $mapper->findOffset();
-if($offset) {
-	echo json_encode($mapper->getMapping($offset));
+foreach($sources as $name => $source) {
+	$logger = new Monolog\Logger('fetch_'.$name);
+	try {
+		$logger->info('Fetching '.$name.' position data from FTP...');
+		$updated = ftp_fetch_if_newer($source['gtfs'], __DIR__.'/data/'.$source['gtfs_file']);
+		if(!$updated) {
+			$logger->info('Nothing to do, remote file not newer than local one');
+			continue;
+		}
+		
+		$logger->info('Fetching '.$name.' positions from TTSS...');
+		fetch($source['ttss'], __DIR__.'/data/'.$source['ttss_file']);
+		
+		$logger->info('Loading data...');
+		$mapper = new Mapper();
+		$mapper->loadTTSS(__DIR__.'/data/'.$source['ttss_file']);
+		$mapper->loadGTFS(__DIR__.'/data/'.$source['gtfs_file']);
+		
+		$logger->info('Finding correct offset...');
+		$offset = $mapper->findOffset();
+		if($offset) {
+			$logger->info('Got offset '.$offset.', creating mapping...');
+			$mapping = $mapper->getMapping($offset);
+			echo json_encode($mapping);
+		}
+		$logger->info('Finished');
+	} catch(Throwable $e) {
+		$logger->error($e->getMessage(), ['exception' => $e, 'exception_string' => (string)$e]);
+	}
 }
