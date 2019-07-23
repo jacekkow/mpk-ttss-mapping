b41a443c0d92114c1ca1e59bb2c978629a08142d
Jacek Kowalski <Jacek@jacekk.info>
2019-07-23 22:59:16 +0200

    Remove "tram2" source from config.php (no longer available)

--- config.php
+++ config.php
@@ -23,18 +23,7 @@ $sources = [
 		'mapper' => $tramTypes,
 		'prefix' => 't',
 	],
-	'tram2' => [
-		'gtfsrt' => 'ftp://ztp.krakow.pl/VehiclePositions.pb',
-		'gtfsrt_file' => 'VehiclePositions_T.pb',
-		'ttss' => 'http://www.ttss.krakow.pl/internetservice/geoserviceDispatcher/services/vehicleinfo/vehicles',
-		'ttss_file' => 'vehicles_T.json',
-		'database' => 'mapping_T.sqlite3',
-		'result' => 'mapping_T.json',
-		'result_vehicles' => 'vehicles_T.html',
-		'mapper' => $tramTypes,
-		'prefix' => 't',
-	],
-]; 
+];
 
 foreach($sources as $name => &$source) {
 	foreach(['gtfsrt_file', 'ttss_file', 'database', 'result', 'result_vehicles'] as $field) {
