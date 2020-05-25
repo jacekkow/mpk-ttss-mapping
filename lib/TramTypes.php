bf4b8f1aadbe87edbae34368cc10d7b6e08ecbca
Jacek Kowalski <Jacek@jacekk.info>
2020-05-25 17:16:53 +0200

    Modify Stadler Tango depot assignment

--- lib/TramTypes.php
+++ lib/TramTypes.php
@@ -37,7 +37,8 @@ class TramTypes extends VehicleTypes {
 615	626	RP	NGT6 (2)	2
 627	650	RP	NGT6 (3)	2
 801	824	RY	NGT8	2
-825	859	RY	Stadler Tango	2
+825	839	RY	Stadler Tango	2
+840	859	HY	Stadler Tango	2
 899	899	RY	126N	2
 901	914	RG	2014N	2
 915	936	HG	2014N	2
