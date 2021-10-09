4d852748e8668e77f06be90775292c36feb5ce93
Jacek Kowalski <Jacek@jacekk.info>
2021-10-09 19:07:49 +0200

    Update GTFS RT positions location (FTP server returnin auth errors)

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $sources = [
 	'bus' => [
-		'gtfsrt' => 'ftp://ztp.krakow.pl/pliki-gtfs/VehiclePositions_A.pb',
+		'gtfsrt' => 'http://gtfs.ztp.krakow.pl/VehiclePositions_A.pb',
 		'gtfsrt_file' => 'VehiclePositions_A.pb',
 		'ttss' => 'http://ttss.mpk.krakow.pl/internetservice/geoserviceDispatcher/services/vehicleinfo/vehicles',
 		'ttss_file' => 'vehicles_A.json',
@@ -12,7 +12,7 @@ $sources = [
 		'prefix' => 'b',
 	],
 	'tram' => [
-		'gtfsrt' => 'ftp://ztp.krakow.pl/pliki-gtfs/VehiclePositions_T.pb',
+		'gtfsrt' => 'http://gtfs.ztp.krakow.pl/VehiclePositions_T.pb',
 		'gtfsrt_file' => 'VehiclePositions_T.pb',
 		'ttss' => 'http://www.ttss.krakow.pl/internetservice/geoserviceDispatcher/services/vehicleinfo/vehicles?positionType=CORRECTED',
 		'ttss_file' => 'vehicles_T.json',
@@ -28,7 +28,7 @@ foreach($sources as $name => &$source) {
 	foreach(['gtfsrt_file', 'ttss_file', 'database', 'result', 'result_vehicles'] as $field) {
 		$source[$field] = __DIR__.'/data/'.$source[$field];
 	}
-	
+
 	$source['result_temp'] = $source['result'].'.tmp';
 	$source['result_vehicles_temp'] = $source['result_vehicles'].'.tmp';
 }
